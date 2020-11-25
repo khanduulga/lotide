281dbf906b2c49c84aec5e38299e4cf39370d247
Khanduulga Munkhbold <m.khanduulga@gmail.com>
2020-11-25 01:09:24 +0000

    compares arrays and returns true if a perfect match, exluding nested arrays and objects

---
+++ eqArrays.js
@@ -0,0 +1,33 @@
+// ASSERTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`🎉️🎉️🎉️ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`👀️👀️👀️ Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+//FUNCTION
+//takes two arrays and returns true if they are a perfect match
+const eqArrays = function(first, second) {
+  if (first.length === second.length) {
+    for (let i = 0; i < first.length; i++) {
+      if (first[i] !== second[i]) {
+        return false;
+      }
+    }
+    return true;
+  } else {
+    return false;
+  }
+};
+
+
+
+
+//TEST CASES
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => true
+assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false); // => false
+
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true); // => true
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false); // => false
