9734eff9b872664dd76ce8b5b574987a7abbd4de
Khanduulga Munkhbold <m.khanduulga@gmail.com>
2020-11-25 21:50:09 +0000

    done with implementation including test cases, counts the reoccurance of a single letter in a string

--- countLetters.js
+++ countLetters.js
@@ -9,20 +9,28 @@ const assertEqual = function(actual, expected) {
 
 
 //FUNCTION
-const countLetter = function(string) {
+const countLetters = function(string) {
   const results = {}
-
+  
   for(const char of string) {
-    if (results[char]) {
-      results[item] += 1;
-    } else {
-      results[char] = 1;
+    if (char !== ' ') {
+      if (results[char]) {
+        results[char] += 1;
+      } else {
+        results[char] = 1;
+      }
     }
   }
-  return results
+  return results;
 }
 
 
 
-
-
+const test1 = countLetters("Lighthouse Rules");
+assertEqual(test1['L'], 1)
+assertEqual(test1['i'], 1)
+assertEqual(test1[' '], undefined)
+assertEqual(test1['R'], 1)
+assertEqual(test1['r'], undefined)
+assertEqual(test1['u'], 2)
+assertEqual(test1['s'], 2)
