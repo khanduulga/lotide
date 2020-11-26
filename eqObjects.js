f00e115ea001886454d695936335aa018de50141
Khanduulga Munkhbold <m.khanduulga@gmail.com>
2020-11-26 00:05:48 +0000

    not complete but compares primatices and arrays in an object

---
+++ eqObjects.js
@@ -0,0 +1,63 @@
+// HELPER FUNCTION
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`🎉️🎉️🎉️ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`👀️👀️👀️ Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+// HELPER FUNCTION
+//takes two arrays and returns true if they are a perfect match
+const eqArrays = function(first, second) {
+  if (first.length === second.length) {
+    for (let i = 0; i < first.length; i++) {
+      if (first[i] !== second[i]) {
+        return false;
+      }
+    }
+    return true;
+  } else {
+    return false;
+  }
+};
+// Returns true if both objects have identical keys with identical values.
+// Otherwise you get back a big fat false!
+const eqObjects = function(object1, object2) {
+  const keys1 = Object.keys(object1);
+  const keys2 = Object.keys(object2);
+
+  if (keys1.length === keys2.length) {
+    for (const key of keys1) {
+      if (Array.isArray(object1[key]) && Array.isArray(object2[key])) {
+        if(!eqArrays(object1[key], object2[key])) {
+          return false;
+        }
+      } else if (object1[key] !== object2[key]) {
+        return false;
+      } 
+    }
+    return true;
+  } else {
+    return false;
+  }
+
+};
+
+
+
+//TEST CASES
+const ab = { a: "1", b: "2" };
+const ba = { b: "2", a: "1" };
+assertEqual(eqObjects(ab, ba), true); // => true
+
+const abc = { a: "1", b: "2", c: "3" };
+assertEqual(eqObjects(ab, abc), false); // => false
+
+
+//test for arrays
+const cd = { c: "1", d: ["2", 3] };
+const dc = { d: ["2", 3], c: "1" };
+assertEqual(eqObjects(cd, dc), true); // => true
+
+const cd2 = { c: "1", d: ["2", 3, 4] };
+assertEqual(eqObjects(cd, cd2), false); // => false
